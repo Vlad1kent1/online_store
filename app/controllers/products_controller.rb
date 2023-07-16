f9e4b3876af70c1548aa2a05ff64dfa7e44e1a32
vladkarabinovych <karabin0347@gmail.com>
2023-07-16 13:11:30 +0300

    remove unnecessary crud files and mothods for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,43 +3,8 @@ class ProductsController < ApplicationController
     @products = collection
   end
 
-  def new
-    @product = Product.new
-  end
-
   def show
     @product = resourse
-  end
-
-  def edit
-    @product = resourse
-  end
-
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      redirect_to @product, notice: "Product was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    @product = resourse
-
-    if @product.update(product_params)
-      redirect_to @product, notice: "Product was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @product = resourse
-    @product.destroy
-    
-    redirect_to products_url, notice: "Product was successfully deleted."
   end
 
   private
@@ -50,9 +15,5 @@ class ProductsController < ApplicationController
 
   def resourse
     collection.find(params[:id])
-  end
-
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :balance)
   end
 end
